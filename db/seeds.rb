2451151b95135a279b3d9b15da4a28b5ae347dbf
Jamie Macey <jamie.git@tracefunc.com>
2021-09-21 11:49:34 -0700

    Article show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,19 @@ Article.create(
   title: "How to build webapps that scale",
   description: "This is the description for the post.",
   favorites_count: 29,
-  body: "",
+  body: <<~HTML,
+    <p>
+    Web development technologies have evolved at an incredible clip over the past few years.
+    </p>
+    <h2 id="introducing-ionic">Introducing RealWorld.</h2>
+    <p>It's a great solution for learning how other frameworks work.</p>
+  HTML
 )
 Article.create(
   title: "The song you won't ever stop singing. No matter how hard you try.",
   description: "This is the description for the post.",
   favorites_count: 32,
-  body: "",
+  body: <<~HTML,
+    <p>This is the song that never ends... Yes it goes on and on my friends...</p>
+  HTML
 )
