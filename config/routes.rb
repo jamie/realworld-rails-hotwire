b50584fa7816a8f6ddd95828f53acf94a9caaaf9
Jamie Macey <jamie.git@tracefunc.com>
2021-10-18 19:03:40 -0700

    Quick stub of Sessions controller for register/login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   resources :articles
   resources :comments
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
