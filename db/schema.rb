27f954900197506589af8d77bd322b4e04bfe58d
Jamie Macey <jamie.git@tracefunc.com>
2021-10-18 17:28:01 -0700

    Revert "devise:install"
    This reverts commit 5b4eff92b667d00bf9c52410d3ecc8d628f16001.
    
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -87,18 +87,6 @@ ActiveRecord::Schema.define(version: 2021_09_21_232858) do
     t.index ["taggings_count"], name: "index_gutentag_tags_on_taggings_count"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
