616067b4df0eab3c15e5e073571d85d9760fb287
Jamie Macey <jamie.git@tracefunc.com>
2021-10-18 19:15:52 -0700

    Move seeds to fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Article.create(
-  title: "How to build webapps that scale",
-  description: "This is the description for the post.",
-  favorites_count: 29,
-  content: <<~HTML,
-    <p>
-    Web development technologies have evolved at an incredible clip over the past few years.
-    </p>
-    <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-    <p>It's a great solution for learning how other frameworks work.</p>
-  HTML
-)
-Article.create(
-  title: "The song you won't ever stop singing. No matter how hard you try.",
-  description: "This is the description for the post.",
-  favorites_count: 32,
-  content: <<~HTML,
-    <p>This is the song that never ends... Yes it goes on and on my friends...</p>
-  HTML
-)
+system "rails db:fixtures:load"
