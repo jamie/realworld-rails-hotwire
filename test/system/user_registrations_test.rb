7c85fb266b49fdcfde65bb22c53945ce70e596af
Jamie Macey <jamie.git@tracefunc.com>
2022-05-20 15:41:28 -0700

    User registration test

--- test/system/user_registrations_test.rb
+++ test/system/user_registrations_test.rb
@@ -1,11 +1,18 @@
 require "application_system_test_case"
 
 class UserRegistrationsTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "signs up" do
     visit root_url
-
     click_on "Sign up"
 
-    assert_selector "h1", text: "Sign up"
+    within ".auth-page" do
+      fill_in :user_username, with: "charlie"
+      fill_in :user_email, with: "charlie@example.com"
+      fill_in :user_password, with: "passwordA1"
+      click_on "Sign up"
+    end
+
+    assert_selector "h1", text: "conduit"
+    assert_selector "a", text: "Sign out" # Proxy to confirm logged in as user
   end
 end
