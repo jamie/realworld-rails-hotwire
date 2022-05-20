ad434f076b9dd207f93e17de7161c0f4d47e65a4
Jamie Macey <jamie.git@tracefunc.com>
2022-05-20 15:47:01 -0700

    Rename app "name"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "realworld_starter_kit_production"
+  # config.active_job.queue_name_prefix = "realworld_production"
 
   config.action_mailer.perform_caching = false
 
