fc848e97fa5084b17ecf583aed001f75dff41a1d
Jamie Macey <jamie.git@tracefunc.com>
2022-05-20 21:18:39 -0700

    clean up session cookie after db reset

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def current_user
     if session[:user_id]
-      User.find(session[:user_id])
+      user = User.find(session[:user_id])
+      session.delete(:user_id) unless user
+      user
     end
   end
 end
