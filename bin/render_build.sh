6bc98425817ee550296a1d61fb5edbee8b5daeb0
Jamie Macey <jamie.git@tracefunc.com>
2022-05-20 20:01:08 -0700

    Deploy config for render.com

---
+++ bin/render_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+mv production.key config/credentials/production.key
+bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
