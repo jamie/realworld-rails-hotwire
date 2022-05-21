60de993b85803925e48a445d88c5a8e422bc0d6c
Jamie Macey <jamie.git@tracefunc.com>
2022-05-20 20:17:58 -0700

    restore assets prep

--- bin/render_build.sh
+++ bin/render_build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 mv production.key config/credentials/production.key
 bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
