6044b1f0c35ef753276110930fb51ad7e76ac511
Jamie Macey <jamie.git@tracefunc.com>
2022-07-31 22:50:02 +0000

    Deploy notification hook

---
+++ bin/notify_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Airbrake
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"environment":"production","username":"render","repository":"https://github.com/jamie/realworld-hotwire-with-rails","revision":"'$RENDER_GIT_COMMIT'"}' \
+  https://api.airbrake.io/api/v4/projects/439210/deploys?key=$AIRBRAKE_PROJECT_KEY
