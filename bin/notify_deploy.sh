c296b8a8aeaeaeb3d43c44fde127760fdd05fd09
Jamie Macey <jamie.git@tracefunc.com>
2022-07-31 22:51:39 +0000

    Dynamic env

--- bin/notify_deploy.sh
+++ bin/notify_deploy.sh
@@ -4,5 +4,5 @@
 curl \
   -X POST \
   -H "Content-Type: application/json" \
-  -d '{"environment":"production","username":"render","repository":"https://github.com/jamie/realworld-hotwire-with-rails","revision":"'$RENDER_GIT_COMMIT'"}' \
+  -d '{"environment":"'$RAILS_ENV'","username":"render","repository":"https://github.com/jamie/realworld-hotwire-with-rails","revision":"'$RENDER_GIT_COMMIT'"}' \
   https://api.airbrake.io/api/v4/projects/439210/deploys?key=$AIRBRAKE_PROJECT_KEY
