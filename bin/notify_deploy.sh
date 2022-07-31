8ad0f994cd2132667fca0d924832787821fbaa4f
Jamie Macey <jamie.git@tracefunc.com>
2022-07-31 22:53:14 +0000

    Use existing deploy script for render deploy

--- bin/notify_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Airbrake
-curl \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"environment":"'$RAILS_ENV'","username":"render","repository":"https://github.com/jamie/realworld-hotwire-with-rails","revision":"'$RENDER_GIT_COMMIT'"}' \
-  https://api.airbrake.io/api/v4/projects/439210/deploys?key=$AIRBRAKE_PROJECT_KEY
--- bin/render_build.sh
+++ bin/render_build.sh
@@ -8,3 +8,10 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:fixtures:load
+
+# Notify airbrake of deploy
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"environment":"'$RAILS_ENV'","username":"render","repository":"https://github.com/'$RENDER_GIT_REPO_SLUG'","revision":"'$RENDER_GIT_COMMIT'"}' \
+  https://api.airbrake.io/api/v4/projects/439210/deploys?key=$AIRBRAKE_PROJECT_KEY
