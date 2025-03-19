aa9ab56aed6392b7d8da7902c0c4b080649d8b3e
Jamie Macey <jamie.git@tracefunc.com>
2025-03-18 22:30:18 -0700

    Render post previews from db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create users
+eric = User.find_or_create_by!(username: "Eric Simons", email: "eric@example.com") do |user|
+  user.password = "password123"
+end
+
+albert = User.find_or_create_by!(username: "Albert Pai", email: "albert@example.com") do |user|
+  user.password = "password123"
+end
+
+# Create posts
+Post.find_or_create_by!(title: "How to build webapps that scale", user: eric) do |post|
+  post.body = "This is the description for the post."
+end
+
+Post.find_or_create_by!(title: "The song you won't ever stop singing. No matter how hard you try.", user: albert) do |post|
+  post.body = "This is the description for the post."
+end
