68496ef5e61eec0c9176d4adfc73a5f6659dc99b
Jamie Macey <jamie.git@tracefunc.com>
2025-03-18 22:36:48 -0700

    Replace actiontext with markdown rendering

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_rich_text :body
+  validates :title, presence: true
+
+  def body_html
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
+    markdown.render(body).html_safe
+  end
 end
